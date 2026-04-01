a0ea3848b0f1f167fc5201fec373f428d4328133
Maciej Kot <mackot1337@gmail.com>
2026-04-01 14:20:35 +0200

    Repository function added

--- booking-service/src/main/java/com/booking/booking/repository/BusinessProfileRepository.java
+++ booking-service/src/main/java/com/booking/booking/repository/BusinessProfileRepository.java
@@ -1,5 +1,6 @@
 package com.booking.booking.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ import com.booking.booking.entity.BusinessProfile;
 @Repository
 public interface BusinessProfileRepository extends JpaRepository<BusinessProfile, Long> {
     Optional<BusinessProfile> findOwnerByOwnerEmail(String email);
+    List<BusinessProfile> findAllByActiveTrue();
 }
